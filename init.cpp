1ca2f83cc8f9e72d89db258e07b67c6dbec1fe58
Hugh Perkins <hughperkins@gmail.com>
2015-06-04 08:25:36 +0800

    remove dead code

--- init.cpp
+++ init.cpp
@@ -23,46 +23,9 @@ extern "C" {
   lua_pushnumber(L, prop.NAME); \
   lua_setfield(L, -2, #NAME);
 
-extern "C" {
-  static int clnn_getDeviceProperties(lua_State *L);
-}
-
-extern "C" {
 static int clnn_getDeviceProperties(lua_State *L)
 {
   cout << "clnn_getDeviceProperties" << endl;
-//  int device = (int)luaL_checknumber(L, 1)-1;
-
-  // switch context to given device so the call to cudaMemGetInfo is for the correct device
-//  int oldDevice;
-//  THCudaCheck(cudaGetDevice(&oldDevice));
-//  THCudaCheck(cudaSetDevice(device));
-
-//  struct cudaDeviceProp prop;
-//  THCudaCheck(cudaGetDeviceProperties(&prop, device));
-//  lua_newtable(L);
-//  SET_DEVN_PROP(canMapHostMemory);
-//  SET_DEVN_PROP(clockRate);
-//  SET_DEVN_PROP(computeMode);
-//  SET_DEVN_PROP(deviceOverlap);
-//  SET_DEVN_PROP(integrated);
-//  SET_DEVN_PROP(kernelExecTimeoutEnabled);
-//  SET_DEVN_PROP(major);
-//  SET_DEVN_PROP(maxThreadsPerBlock);
-//  SET_DEVN_PROP(memPitch);
-//  SET_DEVN_PROP(minor);
-//  SET_DEVN_PROP(multiProcessorCount);
-//  SET_DEVN_PROP(regsPerBlock);
-//  SET_DEVN_PROP(sharedMemPerBlock);
-//  SET_DEVN_PROP(textureAlignment);
-//  SET_DEVN_PROP(totalConstMem);
-//  SET_DEVN_PROP(totalGlobalMem);
-//  SET_DEVN_PROP(warpSize);
-//  SET_DEVN_PROP(pciBusID);
-//  SET_DEVN_PROP(pciDeviceID);
-//  SET_DEVN_PROP(pciDomainID);
-//  SET_DEVN_PROP(maxTexture1D);
-//  SET_DEVN_PROP(maxTexture1DLinear);
 
 //  size_t freeMem;
 //  THCudaCheck(cudaMemGetInfo (&freeMem, NULL));
@@ -72,11 +35,7 @@ static int clnn_getDeviceProperties(lua_State *L)
 //  lua_pushstring(L, prop.name);
 //  lua_setfield(L, -2, "name");
 
-  // restore context
-//  THCudaCheck(cudaSetDevice(oldDevice));
-
   return 1;
-}
 }
 
 //static int cutorch_getState(lua_State *L)
@@ -87,34 +46,10 @@ static int clnn_getDeviceProperties(lua_State *L)
 //  return 1;
 //}
 
-//const char *getDevicePropertiesName = "getDeviceProperties";
-
 static const struct luaL_Reg clnn_stuff__ [] = {
-//  {"synchronize", cutorch_synchronize},
-//  {"getDevice", cutorch_getDevice},
-//  {"deviceReset", cutorch_deviceReset},
-//  {"getDeviceCount", cutorch_getDeviceCount},
   {"getDeviceProperties", clnn_getDeviceProperties},
-//  {"getMemoryUsage", cutorch_getMemoryUsage},
-//  {"setDevice", cutorch_setDevice},
-//  {"seed", cutorch_seed},
-//  {"seedAll", cutorch_seedAll},
-//  {"initialSeed", cutorch_initialSeed},
-//  {"manualSeed", cutorch_manualSeed},
-//  {"manualSeedAll", cutorch_manualSeedAll},
-//  {"getRNGState", cutorch_getRNGState},
-//  {"setRNGState", cutorch_setRNGState},
-//  {"getState", cutorch_getState},
   {NULL, NULL}
 };
-
-struct luaL_Reg makeReg( const char *name, lua_CFunction fn ) {
-    struct luaL_Reg reg;
-    reg.name = name;
-    reg.func = fn;
-    cout << "reg.name" << reg.name <<endl;
-    return reg;
-}
 
 int luaopen_libclnn( lua_State *L ) {
   printf("luaopen_libclnn called :-)\n");
@@ -122,15 +57,7 @@ int luaopen_libclnn( lua_State *L ) {
 
   lua_newtable(L);
 
-//  struct luaL_Reg clnn_stuff[2];
- // cout << "clnn_getDeviceProperties" << (long)clnn_getDeviceProperties << endl;
- // clnn_stuff[0] = makeReg("getDeviceProperties", clnn_getDeviceProperties);
-//  clnn_stuff[0] = makeReg("", 0);
- // clnn_stuff[1] = makeReg(NULL, NULL);
-
   luaL_setfuncs(L, clnn_stuff__, 0);
- // cout << "clnn_stuff[0]->name" << clnn_stuff[0].name << endl;
- // luaL_register(L, NULL, clnn_stuff);
   cout << "setfuncs done" << endl;
 
   THClState* state = (THClState*)malloc(sizeof(THClState));
