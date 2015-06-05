c723aec46e490370789c2b89908ee7bcd54cac80
Hugh Perkins <hughperkins@gmail.com>
2015-06-05 08:39:32 +0800

    slight formatting tweak in test/test.lua

--- test/test.lua
+++ test/test.lua
@@ -41,40 +41,40 @@ end
 print('num devices:', clnn.getDeviceCount())
 
 c = torch.ClStorage()
-print('c1', c)
+print('c1\n', c)
 
 c = torch.ClStorage(3)
-print('c2', c)
+print('c2\n', c)
 c[1] = 5
-print('c3', c)
+print('c3\n', c)
 c[3] = 7
-print('c4', c)
+print('c4\n', c)
 -- print('c' .. c)
 
 c = torch.ClStorage{4,9,2}
-print('c5', c)
+print('c5\n', c)
 
 c:fill(7)
-print('c6', c)
+print('c6\n', c)
 
 a = torch.Storage{1.5, 2.4, 5.3}
-print('a', a)
+print('a\n', a)
 
 b = torch.Storage(3)
 b:copy(a)
-print('bbbbb', b)
+print('bbbbb\n', b)
 
 c:copy(a)
-print('c7', c)
+print('c7\n', c)
 
 c[2] = 21
-print('c8', c)
-print('aaaaaa', a)
+print('c8\n', c)
+print('aaaaaa\n', a)
 a:copy(c)
-print('aaaaaa', a)
+print('aaaaaa\n', a)
 
 d = torch.ClStorage(3)
 d:copy(c)
-print('dddd', d)
+print('dddd\n', d)
 
 
