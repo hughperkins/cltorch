f66c75c4411ed3409c04c6cc1fa4e263841b9ddc
Hugh Perkins <hughperkins@gmail.com>
2015-06-30 08:50:03 +0800

    add test-perf script

---
+++ test/test-perf.lua
@@ -0,0 +1,19 @@
+require 'cltorch'
+
+function test_apply2(its)
+  a = torch.ClTensor(50, 500)
+  a:uniform()
+  b = torch.ClTensor(50, 500)
+  b:uniform()
+  a:add(b)
+  cltorch.dumpTimings()
+  for it=1,its do
+    a:add(b)    
+  end
+  cltorch.dumpTimings()
+end
+
+
+cltorch.setAddFinish(1)
+test_apply2(100)
+
