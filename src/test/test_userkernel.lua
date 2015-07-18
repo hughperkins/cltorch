c8feb80b794af4e525b70d92b539e4c327f5b705
Hugh Perkins <hughperkins@gmail.com>
2015-07-18 13:22:10 +0800

    remove old code from test script

--- src/test/test_userkernel.lua
+++ src/test/test_userkernel.lua
@@ -1,36 +1,20 @@
 require 'cltorch'
 
-local function eval(expression)
-  loadstring('res=' .. expression)()
-  print(expression, res)
-end
-
-eval('cltorch.kernel')
-k = cltorch.kernel()
-eval('k')
-
-t = {input={a='torch.ClTensor'}}
-for k,v in pairs(t.input) do
-  print('k,v', k, v)
-end
-
-eval('torch.ClKernel')
-b = torch.ClKernel({input={nElements='int', input='torch.ClTensor'},output={output='torch.ClTensor'},src=[[
+k = torch.ClKernel({input={nElements='int', input='torch.ClTensor'},output={output='torch.ClTensor'},src=[[
    int linearId = get_global_id(0);  // exciting stuff :-P
    if(linearId < nElements) {
      output_data[linearId] = input_data[linearId] + 3.0f;
    }
 ]]})
-print('b', b)
-b:print()
--- eval('torch.ClKernel({src=' .. src .. '})')
+print('k', k)
+k:print()
 
 x = torch.ClTensor({3,5,2})
 y = torch.ClTensor({6,4,2})
 print('x before\n', x)
 print('y before\n', y)
 
-b:run({nElements=3, input=x, output=y})
+k:run({nElements=3, input=x, output=y})
 
 print('y after\n', y)
 
