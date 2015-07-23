5fcdfe879e0fcbcfa82f932254e64ec796b69c5e
Hugh Perkins <hughperkins@gmail.com>
2015-07-24 07:02:46 +0800

    remove dead code from thclapply.cpp

--- src/lib/THClApply.cpp
+++ src/lib/THClApply.cpp
@@ -13,10 +13,12 @@
 using namespace std;
 
 static std::string get_template();
-// Implementation of copyIgnoringOverlaps, defined after pointwiseApply2.
+
+// Called when we are copying into an overlapping index `dst`, but
+// we don't care which writer wins. Hacky but it works.
 void THClTensor_copyIgnoringOverlaps(THClState* state,
-                                       THClTensor* dst,
-                                       THClTensor* src) {
+                                     THClTensor* dst,
+                                     THClTensor* src) {
   CopyOp op;
   THClTensor_pointwiseApply2(state, dst, src, &op,
                                ReadOnly, // ignore overwrites
@@ -26,51 +28,15 @@ void THClTensor_copyIgnoringOverlaps(THClState* state,
 // Threads per block for our apply kernel
 #define THCL_APPLY_THREADS_PER_BLOCK 32 * 16
 
-// Called when we are copying into an overlapping index `dst`, but
-// we don't care which writer wins. Hacky but it works.
-void THClTensor_copyIgnoringOverlaps(THClState* state,
-                                       THClTensor* dst,
-                                       THClTensor* src);
-
 int getWorkgroupSize(THClState *state, int device) {
   return 64;
-
-//  int workgroupSize = THCL_APPLY_THREADS_PER_BLOCK;
-//  int maxWorkgroupSize = ((easycl::DeviceInfo *)state->deviceInfoByDevice[device])->maxWorkGroupSize;
-//  if( workgroupSize > maxWorkgroupSize ) {
-//    workgroupSize = maxWorkgroupSize;
-//  }
-//  return workgroupSize;
 }
-
 dim3 getApplyBlock(THClState *state, int device) {
   return dim3(getWorkgroupSize(state, device));
 }
-
 bool getApplyGrid(THClState* state, int device, long totalElements, dim3& grid) {
-//  int curDevice = -1;
-//  cudaGetDevice(&curDevice);
-
-//  if (curDevice == -1) {
-//    return false;
-//  }
-
-//  // Assume a reasonable number of SMs if no state is available
-//  int numSM =
-//    state ? state->deviceProperties[curDevice].multiProcessorCount : 15;
-
-  // dont think we can get number of SMs in OpenCL? (at least, not in opencl 1.1?)
-  // just hardcode to 16 for now...
-  // FIXME
-//  int numSM = 16;
-
-//  // 16 warps per block * 4 per SM gives 64 warps per SM at maximum,
-//  // which seems to be a good sweetspot for latency hiding
-//  grid = dim3(mymin(DIVUP(totalElements, (long long) getWorkgroupSize(state, device)),
-//                  4LL * numSM));
   int workgroupSize = getWorkgroupSize(state, device);
   grid = dim3((totalElements + workgroupSize - 1 ) / workgroupSize);
-
   return true;
 }
 
@@ -98,21 +64,14 @@ void kernelLaunch_pointwiseApply1( THClState *state, dim3 grid, dim3 block, int 
     string uniqueName = oss.str();
     TemplatedKernel kernelBuilder(cl);
     kernelBuilder.set("dims1", A);
-//    std::vector<int> dims;
-//    if( A >= 0 ) {
-//      dims.push_back(A);
-//    }
     std::string operation = op->operator1();
     kernelBuilder.set("num_tensors", numTensors);
     kernelBuilder.set("num_scalars", numScalars);
-//    kernelBuilder.set("dims", dims);
     kernelBuilder.set("num_tensor_inputs", numTensors);
     kernelBuilder.set("num_point_tensors", numPointTensors);
     kernelBuilder.set("IndexType", TypeParseTraits<IndexType>::name);
-//    kernelBuilder.set("WarpSize", 32);
     kernelBuilder.set("MAX_CLTORCH_DIMS", MAX_CLTORCH_DIMS);
     kernelBuilder.set("operation", operation);
-//    kernelBuilder.set("include_THClReduceApplyUtils", THClReduceApplyUtils_getKernelTemplate());
     kernel = kernelBuilder.buildKernel( uniqueName, "THClApply.cl", get_template(), "THClTensor_pointwiseApplyD" );
       StatefulTimer::timeCheck("Apply1 compiled");
   }
@@ -202,31 +161,18 @@ void kernelLaunch_pointwiseApply2( THClState *state, dim3 grid, dim3 block, int 
   CLKernel *kernel = 0;
   if( cl->kernelExists(oss.str()) ) {
     kernel = cl->getKernel(oss.str());
-//    StatefulTimer::timeCheck("Apply2 1aa");
   } else {
     string uniqueName = oss.str();
-//    StatefulTimer::timeCheck("Apply2 1a");
     TemplatedKernel kernelBuilder(cl);
     kernelBuilder.set("dims1", A);
     kernelBuilder.set("dims2", B);
-//    std::vector<int> dims;
-////    StatefulTimer::timeCheck("Apply2 1b");
-//    if( A >= 0 ) {
-//      dims.push_back(A);
-//    }
-//    if( B != A && B >= 0 ) {
-//      dims.push_back(B);
-//    }
     std::string operation = op->operator2();
     kernelBuilder.set("num_tensors", numTensors);
     kernelBuilder.set("num_scalars", numScalars);
-//    kernelBuilder.set("dims", dims);
     kernelBuilder.set("num_point_tensors", numPointTensors);
     kernelBuilder.set("MAX_CLTORCH_DIMS", MAX_CLTORCH_DIMS);
     kernelBuilder.set("IndexType", TypeParseTraits<IndexType>::name);
-//    kernelBuilder.set("WarpSize", 32);
     kernelBuilder.set("operation", operation);
-//    kernelBuilder.set("include_THClReduceApplyUtils", THClReduceApplyUtils_getKernelTemplate());
     try {
       kernel = kernelBuilder.buildKernel( uniqueName, uniqueName, get_template(), "THClTensor_pointwiseApplyD" );
     } catch( std::runtime_error &e ) {
@@ -262,9 +208,7 @@ void kernelLaunch_pointwiseApply2( THClState *state, dim3 grid, dim3 block, int 
     throw std::runtime_error("Error: out of bounds for totalelements=" + easycl::toString(totalElements));
   }
   k.in( (int)totalElements );
-//  StatefulTimer::timeCheck("Apply2 6");
   k.run(grid, block);
-//  StatefulTimer::timeCheck("Apply2 7");
 
   if(state->addFinish) cl->finish();
   if(StatefulTimer::enabled) StatefulTimer::timeCheck(("Apply2 END " + oss.str()).c_str());
@@ -354,25 +298,12 @@ void kernelLaunch_pointwiseApply3( THClState *state, dim3 grid, dim3 block, int 
     kernelBuilder.set("dims1", A);
     kernelBuilder.set("dims2", B);
     kernelBuilder.set("dims3", C);
-//    std::vector<int> dims;
-//    if( A >= 0 ) {
-//      dims.push_back(A);
-//    }
-//    if( B != A && B >= 0 ) {
-//      dims.push_back(B);
-//    }
-//    if( C != A && C != B && C >= 0 ) {
-//      dims.push_back(C);
-//    }
     std::string operation = op->operator3();
     kernelBuilder.set("num_tensors", numTensors);
     kernelBuilder.set("num_scalars", numScalars);
     kernelBuilder.set("num_point_tensors", numPointTensors);
-//    kernelBuilder.set("dims", dims);
     kernelBuilder.set("IndexType", TypeParseTraits<IndexType>::name);
-//    kernelBuilder.set("WarpSize", 32);
     kernelBuilder.set("MAX_CLTORCH_DIMS", MAX_CLTORCH_DIMS);
-//    kernelBuilder.set("include_THClReduceApplyUtils", THClReduceApplyUtils_getKernelTemplate());
     kernelBuilder.set("operation", operation);
     kernel = kernelBuilder.buildKernel( uniqueName, uniqueName, get_template(), "THClTensor_pointwiseApplyD" );
 //    cout << kernelBuilder.getRenderedKernel(get_template()) << endl;
@@ -402,7 +333,6 @@ void kernelLaunch_pointwiseApply3( THClState *state, dim3 grid, dim3 block, int 
     k.in((int)cInfo.strides[d]);
   }
   k.in(cInfo.wrapper);
-//  StatefulTimer::timeCheck("Apply3 b");
   for( int i = 0; i < numScalars; i++ ) {
     k.in(hasScalars->getScalar(i));
   }
@@ -413,7 +343,6 @@ void kernelLaunch_pointwiseApply3( THClState *state, dim3 grid, dim3 block, int 
     throw std::runtime_error("Error: out of bounds for totalelements=" + easycl::toString(totalElements));
   }
   k.in( (int)totalElements );
-//  StatefulTimer::timeCheck("Apply3 c");
   k.run(grid, block);
 
   if(state->addFinish) cl->finish();
@@ -431,8 +360,7 @@ bool THClTensor_pointwiseApply1(THClState* state,
     return false;
   }
 
-  if (THClTensor_nDimension(state, a) == 0) {
-    // Zero-dim tensor; do nothing
+  if (THClTensor_nDimension(state, a) == -1) {
     return true;
   }
 
@@ -453,17 +381,11 @@ bool THClTensor_pointwiseApply1(THClState* state,
   // an error, since it is unclear which one should win), but we will
   // preserve this last-writer-wins (in arbitrary copy order) behavior.
   THClTensor* oldA = NULL;
-
   if (aType == ReadWrite && THCL_overlappingIndices(state, a)) {
-    // Must perform in contiguous space
     oldA = a;
     a = THClTensor_newContiguous(state, a);
   }
 
-  // Can we use 32-bit integer math in the kernel (the linear ID for the copy
-  // and the resulting non-linear offset is all computable using 32-bit math?)
-  // We also use unsigned index math in the kernel, as signed div/mod has
-  // additional overhead.
   if (THCL_canUse32BitIndexMath(state, a)) {
     TensorInfo<unsigned int> aInfo(state, a);
 
@@ -473,26 +395,13 @@ bool THClTensor_pointwiseApply1(THClState* state,
   } else {
     TensorInfo<unsigned long> aInfo(state, a);
 
-    // For large tensors, we only compile the completely contiguous
-    // version and the completely generic version, to reduce
-    // compilation time.
     if (aInfo.isContiguous()) {
-      /*THClTensor_pointwiseApply1<Op, unsigned long, -2>
-        <<<grid, block, 0, THClState_getCurrentStream(state)>>>(
-          aInfo, (unsigned long) totalElements, op);*/
       THError("Not implemented");
     } else {
-      /*THClTensor_pointwiseApply1<Op, unsigned long, -1>
-        <<<grid, block, 0, THClState_getCurrentStream(state)>>>(
-          aInfo, (unsigned long) totalElements, op);*/
       THError("Not implemented");
     }
   }
-
   if (oldA) {
-    // Ignore overlaps when copying back; if we use THClTensor_copy
-    // instead, it will recursively try and invoke ourselves to make
-    // oldA contiguous.
     THClTensor_copyIgnoringOverlaps(state, oldA, a);
     THClTensor_free(state, a);
     a = oldA;
@@ -521,8 +430,7 @@ bool THClTensor_pointwiseApply2(THClState* state,
     return false;
   }
 
-  if (THClTensor_nDimension(state, a) == 0) {
-    // Zero-dim tensor; do nothing
+  if (THClTensor_nDimension(state, a) == -1) {
     return true;
   }
 
@@ -547,30 +455,19 @@ bool THClTensor_pointwiseApply2(THClState* state,
   THClTensor* oldB = NULL;
 
   if (aType == ReadWrite && THCL_overlappingIndices(state, a)) {
-    // Must perform in contiguous space
     oldA = a;
     a = THClTensor_newContiguous(state, a);
   }
   if (bType == ReadWrite && THCL_overlappingIndices(state, b)) {
-    // Must perform in contiguous space
     oldB = b;
     b = THClTensor_newContiguous(state, b);
   }
-
 
   if (THCL_canUse32BitIndexMath(state, a) &&
       THCL_canUse32BitIndexMath(state, b)) {
     TensorInfo<unsigned int> aInfo(state, a);
     TensorInfo<unsigned int> bInfo(state, b);
 
-    // It is possible that the tensor dimensions are able to be collapsed,
-    // and thus we can reduce the actual code complexity of the copy by
-    // exploiting this knowledge statically, since the div/mod is the
-    // most expensive part of the operation, more so than memory accesses.
-    // For instance, when copying a non-contiguous to a contiguous tensor
-    // (or vice versa), the contiguous tensor can be collapsed to one
-    // dimension, and the loop to translate the linear index to the array
-    // index can be similarly collapsed. That is what this unrolling is for.
     int A = aInfo.dims;
     int B = bInfo.dims;
     if(aInfo.isContiguous()) A = -2;
@@ -581,35 +478,19 @@ bool THClTensor_pointwiseApply2(THClState* state,
     TensorInfo<unsigned long> aInfo(state, a);
     TensorInfo<unsigned long> bInfo(state, b);
 
-    // For large tensors, we only compile the completely contiguous
-    // version and the completely generic version, to reduce
-    // compilation time.
     if (aInfo.isContiguous() && bInfo.isContiguous()) {
       THError("Not implemented");
-//      THClTensor_pointwiseApply2<Op, unsigned long, -2, -2>
-//        <<<grid, block, 0, THClState_getCurrentStream(state)>>>(
-//          aInfo, bInfo, (unsigned long) totalElements, op);
     } else {
       THError("Not implemented");
-//      THClTensor_pointwiseApply2<Op, unsigned long, -1, -1>
-//        <<<grid, block, 0, THClState_getCurrentStream(state)>>>(
-//          aInfo, bInfo, (unsigned long) totalElements, op);
     }
   }
 
   if (oldA) {
-    // Ignore overlaps when copying back; if we use THClTensor_copy
-    // instead, it will recursively try and invoke ourselves to make
-    // oldA contiguous.
     THClTensor_copyIgnoringOverlaps(state, oldA, a);
     THClTensor_free(state, a);
     a = oldA;
   }
-
   if (oldB) {
-    // Ignore overlaps when copying back; if we use THClTensor_copy
-    // instead, it will recursively try and invoke ourselves to make
-    // oldB contiguous.
     THClTensor_copyIgnoringOverlaps(state, oldB, b);
     THClTensor_free(state, b);
     b = oldB;
@@ -641,8 +522,7 @@ bool THClTensor_pointwiseApply3(THClState* state,
     return false;
   }
 
-  if (THClTensor_nDimension(state, a) == 0) {
-    // Zero-dim tensor; do nothing
+  if (THClTensor_nDimension(state, a) == -1) {
     return true;
   }
 
@@ -668,19 +548,14 @@ bool THClTensor_pointwiseApply3(THClState* state,
   THClTensor* oldC = NULL;
 
   if (aType == ReadWrite && THCL_overlappingIndices(state, a)) {
-    // Must perform in contiguous space
     oldA = a;
     a = THClTensor_newContiguous(state, a);
   }
-
   if (bType == ReadWrite && THCL_overlappingIndices(state, b)) {
-    // Must perform in contiguous space
     oldB = b;
     b = THClTensor_newContiguous(state, b);
   }
-
   if (cType == ReadWrite && THCL_overlappingIndices(state, c)) {
-    // Must perform in contiguous space
     oldC = c;
     c = THClTensor_newContiguous(state, c);
   }
@@ -703,49 +578,28 @@ bool THClTensor_pointwiseApply3(THClState* state,
     TensorInfo<unsigned long> bInfo(state, b);
     TensorInfo<unsigned long> cInfo(state, c);
 
-    // For large tensors, we only compile the completely contiguous
-    // version and the completely generic version, to reduce
-    // compilation time.
     if (aInfo.isContiguous() && bInfo.isContiguous() && cInfo.isContiguous()) {
       THError("Not implemented");
-//      THClTensor_pointwiseApply3<Op, unsigned long, -2, -2, -2>
-//        <<<grid, block, 0, THClState_getCurrentStream(state)>>>(
-//          aInfo, bInfo, cInfo, (unsigned long) totalElements, op);
     } else {
       THError("Not implemented");
-//      THClTensor_pointwiseApply3<Op, unsigned long, -1, -1, -1>
-//        <<<grid, block, 0, THClState_getCurrentStream(state)>>>(
-//          aInfo, bInfo, cInfo, (unsigned long) totalElements, op);
     }
   }
 
   if (oldA) {
-    // Ignore overlaps when copying back; if we use THClTensor_copy
-    // instead, it will recursively try and invoke ourselves to make
-    // oldA contiguous.
     THClTensor_copyIgnoringOverlaps(state, oldA, a);
     THClTensor_free(state, a);
     a = oldA;
   }
-
   if (oldB) {
-    // Ignore overlaps when copying back; if we use THClTensor_copy
-    // instead, it will recursively try and invoke ourselves to make
-    // oldB contiguous.
     THClTensor_copyIgnoringOverlaps(state, oldB, b);
     THClTensor_free(state, b);
     b = oldB;
   }
-
   if (oldC) {
-    // Ignore overlaps when copying back; if we use THClTensor_copy
-    // instead, it will recursively try and invoke ourselves to make
-    // oldC contiguous.
     THClTensor_copyIgnoringOverlaps(state, oldC, c);
     THClTensor_free(state, c);
     c = oldC;
   }
-
   return true;
 }
 
@@ -822,8 +676,6 @@ std::string get_template() {
   "   int totalElements) {\n" 
   "   int linearIndex = get_global_id(0);\n" 
   "   if(linearIndex < totalElements ) {\n" 
-  "      {{IndexType}} curDimIndex;\n" 
-  "      {{IndexType}} curDimOffset;\n" 
   "      int thisLinearId;\n" 
   "    {% for t=1,num_tensors do %}\n" 
   "      {% local thisdims = loadstring('return dims' .. t)() %}\n" 
@@ -833,9 +685,7 @@ std::string get_template() {
   "         {{IndexType}} derived_offset_{{t}} = offset_{{t}};\n" 
   "         thisLinearId = linearIndex;\n" 
   "        {% for d=thisdims,1,-1 do %}  // bake this in....\n" 
-  "          curDimIndex = thisLinearId % size_{{t}}_{{d}};\n" 
-  "          curDimOffset = curDimIndex * stride_{{t}}_{{d}};\n" 
-  "          derived_offset_{{t}} += curDimOffset;\n" 
+  "          derived_offset_{{t}} += (thisLinearId % size_{{t}}_{{d}}) * stride_{{t}}_{{d}};\n" 
   "          {% if d > 0 then %}\n" 
   "            thisLinearId /= size_{{t}}_{{d}};\n" 
   "          {% end %}\n" 
