b2d368d17006db9b64eb2ff41a3472ceab6e75ad
Hugh Perkins <hughperkins@gmail.com>
2016-04-13 11:37:49 +0200

    start pondering porting sort

---
+++ src/lib/THClTensorSort.h
@@ -0,0 +1,24 @@
+// from lib/THC/THCTensorSort.h:
+
+#ifndef TH_CL_TENSOR_SORT_INC
+#define TH_CL_TENSOR_SORT_INC
+
+#include "THClTensor.h"
+
+/* Performs an in-place sort of (keys, values). Only works for slice sizes
+   <= 2048 at the moment (slice size == size of keys/values dim `dim`) */
+THCL_API void THClTensor_sortKeyValueInplace(THClState* state,
+                                              THClTensor* keys,
+                                              THClTensor* values,
+                                              int dim, int order);
+
+/* Performs an out-of-place sort of `input`, returning the per-slice indices
+   in `indices` and the sorted values in `sorted` */
+THCL_API void THClTensor_sort(THClState* state,
+                               THClTensor* sorted,
+                               THClTensor* indices,
+                               THClTensor* input,
+                               int dim, int order);
+
+#endif
+
