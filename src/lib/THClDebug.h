177098d9becd0e9caf55b40afd1c126353766f68
Hugh Perkins <hughperkins@gmail.com>
2016-04-23 21:18:51 +0200

    add THClDebug_printSize(name, ...)

--- src/lib/THClDebug.h
+++ src/lib/THClDebug.h
@@ -4,4 +4,5 @@
 
 void THClDebug_printTensor(THClState *state, THClTensor *target);
 void THClDebug_printSize(THClState *state, THClTensor *target);
+void THClDebug_printSize(const char *name, THClState *state, THClTensor *target);
 
