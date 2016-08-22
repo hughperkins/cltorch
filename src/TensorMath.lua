855e620b8f3149ccd9235c3108737ed7017f3803
Hugh Perkins <hughperkins@gmail.com>
2016-08-22 14:57:50 +0100

    add bytetensor to tEnsormath.lua

--- src/TensorMath.lua
+++ src/TensorMath.lua
@@ -232,6 +232,309 @@ wrap.types.LongArg = {
    end
 }
 
+-- wrap.types.ByteTensor = {
+   
+--    vararg = true,
+   
+--    helpname = function(arg)
+--       return "(ByteTensor | dim1 [dim2...])"
+--    end,
+   
+--    declare = function(arg)
+--       return string.format("THByteTensor *arg%d = NULL;", arg.i)
+--    end,
+   
+--    init = function(arg)
+--       if arg.default then
+--          error('ByteTensor cannot have a default value')
+--       end
+--    end,
+   
+--    -- check = function(arg, idx)
+--    --    return string.format("torch_isbytetensor(L, %d)", idx)
+--    -- end,
+   
+--    -- read = function(arg, idx)
+--    --    return string.format("arg%d = torch_checkbytetensors(L, %d);", arg.i, idx)
+--    -- end,
+   
+--    carg = function(arg, idx)
+--       return string.format('arg%d', arg.i)
+--    end,
+   
+--    creturn = function(arg, idx)
+--       return string.format('arg%d', arg.i)
+--    end,
+   
+--    precall = function(arg)
+--       local txt = {}
+--       if arg.returned then
+--          table.insert(txt, string.format('luaT_pushudata(L, arg%d, "torch.ByteTensor");', arg.i))
+--       end
+--       return table.concat(txt, '\n')
+--    end,
+   
+--    postcall = function(arg)
+--       local txt = {}
+--       if arg.creturned then
+--          -- this next line is actually debatable
+--          table.insert(txt, string.format('THByteTensor_retain(arg%d);', arg.i))
+--          table.insert(txt, string.format('luaT_pushudata(L, arg%d, "torch.ByteTensor");', arg.i))
+--       end
+--       if not arg.returned and not arg.creturned then
+--          table.insert(txt, string.format('THByteTensor_free(arg%d);', arg.i))
+--       end
+--       return table.concat(txt, '\n')
+--    end
+-- }
+
+
+-- === from torch's torchcwrap.lua VVVVV  ================
+
+wrap.types.Tensor = {
+
+   helpname = function(arg)
+                 if arg.dim then
+                    return string.format("Tensor~%dD", arg.dim)
+                 else
+                    return "Tensor"
+                 end
+            end,
+
+   declare = function(arg)
+                local txt = {}
+                table.insert(txt, string.format("THTensor *arg%d = NULL;", arg.i))
+                if arg.returned then
+                   table.insert(txt, string.format("int arg%d_idx = 0;", arg.i));
+                end
+                return table.concat(txt, '\n')
+           end,
+
+   check = function(arg, idx)
+              if arg.dim then
+                 return string.format("(arg%d = luaT_toudata(L, %d, torch_Tensor)) && (arg%d->nDimension == %d)", arg.i, idx, arg.i, arg.dim)
+              else
+                 return string.format("(arg%d = luaT_toudata(L, %d, torch_Tensor))", arg.i, idx)
+              end
+         end,
+
+   read = function(arg, idx)
+             if arg.returned then
+                return string.format("arg%d_idx = %d;", arg.i, idx)
+             end
+          end,
+
+   init = function(arg)
+             if type(arg.default) == 'boolean' then
+                return string.format('arg%d = THTensor_(new)();', arg.i)
+             elseif type(arg.default) == 'number' then
+                return string.format('arg%d = %s;', arg.i, arg.args[arg.default]:carg())
+             else
+                error('unknown default tensor type value')
+             end
+          end,
+
+   carg = function(arg)
+             return string.format('arg%d', arg.i)
+          end,
+
+   creturn = function(arg)
+                return string.format('arg%d', arg.i)
+             end,
+
+   precall = function(arg)
+                local txt = {}
+                if arg.default and arg.returned then
+                   table.insert(txt, string.format('if(arg%d_idx)', arg.i)) -- means it was passed as arg
+                   table.insert(txt, string.format('lua_pushvalue(L, arg%d_idx);', arg.i))
+                   table.insert(txt, string.format('else'))
+                   if type(arg.default) == 'boolean' then -- boolean: we did a new()
+                      table.insert(txt, string.format('luaT_pushudata(L, arg%d, torch_Tensor);', arg.i))
+                   else  -- otherwise: point on default tensor --> retain
+                      table.insert(txt, string.format('{'))
+                      table.insert(txt, string.format('THTensor_(retain)(arg%d);', arg.i)) -- so we need a retain
+                      table.insert(txt, string.format('luaT_pushudata(L, arg%d, torch_Tensor);', arg.i))
+                      table.insert(txt, string.format('}'))
+                   end
+                elseif arg.default then
+                   -- we would have to deallocate the beast later if we did a new
+                   -- unlikely anyways, so i do not support it for now
+                   if type(arg.default) == 'boolean' then
+                      error('a tensor cannot be optional if not returned')
+                   end
+                elseif arg.returned then
+                   table.insert(txt, string.format('lua_pushvalue(L, arg%d_idx);', arg.i))
+                end
+                return table.concat(txt, '\n')
+             end,
+
+   postcall = function(arg)
+                 local txt = {}
+                 if arg.creturned then
+                    -- this next line is actually debatable
+                    table.insert(txt, string.format('THTensor_(retain)(arg%d);', arg.i))
+                    table.insert(txt, string.format('luaT_pushudata(L, arg%d, torch_Tensor);', arg.i))
+                 end
+                 return table.concat(txt, '\n')
+              end
+}
+
+for _,typename in ipairs({"ByteTensor", "CharTensor", "ShortTensor", "IntTensor", "LongTensor",
+                          "FloatTensor", "DoubleTensor"}) do
+
+   wrap.types[typename] = {
+
+      helpname = function(arg)
+                    if arg.dim then
+                       return string.format('%s~%dD', typename, arg.dim)
+                    else
+                       return typename
+                    end
+                 end,
+
+      declare = function(arg)
+                   local txt = {}
+                   table.insert(txt, string.format("TH%s *arg%d = NULL;", typename, arg.i))
+                   if arg.returned then
+                      table.insert(txt, string.format("int arg%d_idx = 0;", arg.i));
+                   end
+                   return table.concat(txt, '\n')
+                end,
+
+      check = function(arg, idx)
+                 if arg.dim then
+                    return string.format('(arg%d = luaT_toudata(L, %d, "torch.%s")) && (arg%d->nDimension == %d)', arg.i, idx, typename, arg.i, arg.dim)
+                 else
+                    return string.format('(arg%d = luaT_toudata(L, %d, "torch.%s"))', arg.i, idx, typename)
+                 end
+              end,
+
+      read = function(arg, idx)
+                if arg.returned then
+                   return string.format("arg%d_idx = %d;", arg.i, idx)
+                end
+             end,
+
+      init = function(arg)
+                if type(arg.default) == 'boolean' then
+                   return string.format('arg%d = TH%s_new();', arg.i, typename)
+                elseif type(arg.default) == 'number' then
+                   return string.format('arg%d = %s;', arg.i, arg.args[arg.default]:carg())
+                else
+                   error('unknown default tensor type value')
+                end
+             end,
+
+      carg = function(arg)
+                return string.format('arg%d', arg.i)
+             end,
+
+      creturn = function(arg)
+                   return string.format('arg%d', arg.i)
+             end,
+
+      precall = function(arg)
+                   local txt = {}
+                   if arg.default and arg.returned then
+                      table.insert(txt, string.format('if(arg%d_idx)', arg.i)) -- means it was passed as arg
+                      table.insert(txt, string.format('lua_pushvalue(L, arg%d_idx);', arg.i))
+                      table.insert(txt, string.format('else'))
+                      if type(arg.default) == 'boolean' then -- boolean: we did a new()
+                         table.insert(txt, string.format('luaT_pushudata(L, arg%d, "torch.%s");', arg.i, typename))
+                      else  -- otherwise: point on default tensor --> retain
+                         table.insert(txt, string.format('{'))
+                         table.insert(txt, string.format('TH%s_retain(arg%d);', typename, arg.i)) -- so we need a retain
+                         table.insert(txt, string.format('luaT_pushudata(L, arg%d, "torch.%s");', arg.i, typename))
+                         table.insert(txt, string.format('}'))
+                      end
+                   elseif arg.default then
+                      -- we would have to deallocate the beast later if we did a new
+                      -- unlikely anyways, so i do not support it for now
+                      if type(arg.default) == 'boolean' then
+                         error('a tensor cannot be optional if not returned')
+                      end
+                   elseif arg.returned then
+                      table.insert(txt, string.format('lua_pushvalue(L, arg%d_idx);', arg.i))
+                   end
+                   return table.concat(txt, '\n')
+                end,
+
+      postcall = function(arg)
+                    local txt = {}
+                    if arg.creturned then
+                       -- this next line is actually debatable
+                       table.insert(txt, string.format('TH%s_retain(arg%d);', typename, arg.i))
+                       table.insert(txt, string.format('luaT_pushudata(L, arg%d, "torch.%s");', arg.i, typename))
+                    end
+                    return table.concat(txt, '\n')
+                 end
+   }
+
+   wrap.types[typename .. 'Array'] = {
+
+      helpname = function(arg)
+                    return string.format('{%s+}', typename)
+               end,
+
+      declare = function(arg)
+                   local txt = {}
+                   table.insert(txt, string.format('TH%s **arg%d_data = NULL;', typename, arg.i))
+                   table.insert(txt, string.format('long arg%d_size = 0;', arg.i))
+                   table.insert(txt, string.format('int arg%d_i = 0;', arg.i))
+                   return table.concat(txt, '\n')
+              end,
+
+      check = function(arg, idx)
+                 return string.format('torch_isnonemptytable(L, %d)', idx)
+            end,
+
+      read = function(arg, idx)
+                local txt = {}
+                -- Iterate over the array to find its length, leave elements on stack.
+                table.insert(txt, string.format('do'))
+                table.insert(txt, string.format('{'))
+                table.insert(txt, string.format('  arg%d_size++;', arg.i))
+                table.insert(txt, string.format('  lua_checkstack(L, 1);'))
+                table.insert(txt, string.format('  lua_rawgeti(L, %d, arg%d_size);', idx, arg.i))
+                table.insert(txt, string.format('}'))
+                table.insert(txt, string.format('while (!lua_isnil(L, -1));'))
+                table.insert(txt, string.format('arg%d_size--;', arg.i))
+                -- Pop nil element from stack.
+                table.insert(txt, string.format('lua_pop(L, 1);'))
+                -- Allocate tensor pointers and read values from stack backwards.
+                table.insert(txt, string.format('arg%d_data = (TH%s**)THAlloc(arg%d_size * sizeof(TH%s*));', arg.i, typename, arg.i, typename))
+                table.insert(txt, string.format('for (arg%d_i = arg%d_size - 1; arg%d_i >= 0; arg%d_i--)', arg.i, arg.i, arg.i, arg.i))
+                table.insert(txt, string.format('{'))
+                table.insert(txt, string.format('  if (!(arg%d_data[arg%d_i] = luaT_toudata(L, -1, "torch.%s")))', arg.i, arg.i, typename))
+                table.insert(txt, string.format('    luaL_error(L, "expected %s in tensor array");', typename))
+                table.insert(txt, string.format('  lua_pop(L, 1);'))
+                table.insert(txt, string.format('}'))
+                table.insert(txt, string.format(''))
+                return table.concat(txt, '\n')
+             end,
+
+      init = function(arg)
+             end,
+
+      carg = function(arg)
+                return string.format('arg%d_data,arg%d_size', arg.i, arg.i)
+             end,
+
+      creturn = function(arg)
+                   error('TensorArray cannot be returned.')
+                end,
+
+      precall = function(arg)
+                end,
+
+      postcall = function(arg)
+                    return string.format('THFree(arg%d_data);', arg.i)
+                 end
+   }
+end
+
+-- === from torch's torchcwrap.lua ^^^^  ================
+
 function interface.luaname2wrapname(self, name)
    return string.format('cltorch_ClTensor_%s', name)
 end
@@ -468,11 +771,19 @@ wrap("maskedCopy",
       {name=Tensor},
       {name=Tensor}})
 
+-- require 'torch'
+-- print('Tensor', Tensor)
+-- print('Tensor', Tensor)
 wrap("maskedSelect",
    cname("maskedSelect"),
    {{name=Tensor, returned=true, default=true},
       {name=Tensor},
-      {name=Tensor}})
+      {name=Tensor}},
+   cname("maskedSelectByte"),
+   {{name=Tensor, returned=true, default=true},
+      {name=Tensor},
+      {name="ByteTensor"}}
+)
 
 --wrap("sort",
 --     cname("sort"),
